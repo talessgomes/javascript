eede4ef83493112454394e894f327650dfec451b
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-03 14:57:29 -0300

    finalizado-exercicio-58

---
+++ ex58/index.js
@@ -0,0 +1,11 @@
+function welcomeToBooleans() {
+  // Only change code below this line
+  return true; //Change this line
+
+  // Only change code above this line
+}
+
+welcomeToBooleans();
+
+
+// O valor retornado vai ser true
