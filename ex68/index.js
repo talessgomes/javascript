bfaacdd9463cc7a9f4ec7db49ad7bfd9f6d11d19
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-06 18:37:42 -0300

    finalizado-exercicio-68

---
+++ ex68/index.js
@@ -0,0 +1,13 @@
+function testLessOrEqual(val) {
+    if (val <= 12) { // Change this line
+        return "Smaller than or Equal to 12";
+    }
+
+    if (val <= 24) { // Change this line
+        return "smaller than or Equal to 24";
+    }
+
+    return "More Than 24";
+}
+
+testLessOrEqual(10);
