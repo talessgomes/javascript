e36134ad65f17777cf88576b2b63dd9596b142bf
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-09 15:26:20 -0300

    finalizado-exericio-84

---
+++ ex84/index.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Noah",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Tales", "Isabelly"]
+    // Only change code above this line
+};
