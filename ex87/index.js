3dfd89b72dd79b06aca4ecbab788527bf59e9466
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-09 16:53:20 -0300

    finalizado-exercicio-87

---
+++ ex87/index.js
@@ -0,0 +1,13 @@
+// Setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitar"
+};
+
+// Only change code below this line
+const playerNumber = 16;
+
+const player = testObj[playerNumber];
+
+console.log(player); //Montana
