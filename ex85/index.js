bca10b3d6a1316348cf5ba41f1cfe87cde811272
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-09 15:34:25 -0300

    finalizado-exercicio-85

---
+++ ex85/index.js
@@ -0,0 +1,16 @@
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;
+
+const shirtValue = testObj.shirt;
+
+console.log(hatValue);
+//O valor vai ser ballcap
+console.log(shirtValue);
+//o valor vai ser jersey
