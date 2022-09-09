1f7be33487c1ae5c0e066223f4566f03d9609757
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-09 17:09:56 -0300

    finalizado-exercicio-89

---
+++ ex89/index.js
@@ -0,0 +1,19 @@
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+
+console.log(myDog);
+/*Resposta
+{ name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: [ 'freeCodeCamp Campers' ],
+  bark: 'woof' }
+
+  bark foi adicionado
+*/
