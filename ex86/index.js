54bdeab2a16083bd47afe92eab64c89e22aaebdc
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-09 16:45:58 -0300

    finalizado-exercicio-86

---
+++ ex86/index.js
@@ -0,0 +1,15 @@
+// Setup
+
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];
+
+const drinkValue = testObj["the drink"];
+
+console.log(entreeValue); //hamburger
+console.log(drinkValue); //water
