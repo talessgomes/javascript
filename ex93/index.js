7b4253c3f2f2a4c204537c8edeee6a60a53cddd8
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-09 19:37:38 -0300

    finalizado-exercicio-93

---
+++ ex93/index.js
@@ -0,0 +1,27 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Daft Punk",
+        "title": "Homework",
+        "release_year": 1997,
+        "formats": [
+            "CD",
+            "Cassette",
+            "LP"
+        ],
+        "gold": true
+    }
+];
+
+console.log(myMusic);
+//Agora tem dois albuns em myMusic
