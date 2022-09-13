90106d060cd4cf37423b5f1979099df5b4b23ea6
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-12 21:54:48 -0300

    finalizado-exercicio-97

---
+++ ex97/index.js
@@ -0,0 +1,15 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 0;
+
+while (i <= 5) {
+    myArray.push(i);
+    i++;
+}
+
+console.log(myArray);
+
+/*Estou fazendo um looping, o meu while vai ficar rodando
+até que a variável I seja igual ao número 5 */
