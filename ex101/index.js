5f4ea1cfc9ec7884d1b4f2d8d5f391b93f7326a1
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-13 18:48:51 -0300

    finalizado-exercicio-101

---
+++ ex101/index.js
@@ -0,0 +1,16 @@
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+}
+
+/*outra forma de resolver
+  total = myArr.length * i;
+   */
+
+  /*O looping vai multiplicar a quantidade de número que tem 
+dentro do array pela variável i e vai jogar o valor */
