e6c1fac72d0c77762058cd121017d306993d9492
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-14 20:49:12 -0300

    finalizado-exercicio-107

---
+++ ex107/index.js
@@ -0,0 +1,10 @@
+function randomWholeNum() {
+
+    // Only change code below this line
+
+    return Math.floor(Math.random() * 10);
+}
+
+console.log(randomWholeNum());
+  // pegando o valor retornado da função
+  //número aleatórios
