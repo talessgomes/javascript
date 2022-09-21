c25c814de4ce0c5c98b673f6fb67b530054a7a40
Tales gomes <tales.gomes_sl@hotmail.com>
2022-09-20 23:28:08 -0300

    finalizado-exercicio-113

---
+++ ex113/index.js
@@ -0,0 +1,28 @@
+// Only change code below this line
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const countArray = countdown(n - 1);
+        countArray.unshift(n);
+        return countArray;
+    }
+}
+
+console.log(countdown(5));
+  // Only change code above this line
+
+/*Example
+ 
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
+[1, 2, 3, 4, 5]
+ */
